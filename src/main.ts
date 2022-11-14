7beede82fbdc671e53ffeddb87126a0c476c3afe
sonlam137216 <19522133@gm.uit.edu.vn>
2022-11-14 16:13:37 +0700

    init

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const { PORT } = process.env;
+  try {
+    await app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+bootstrap();
